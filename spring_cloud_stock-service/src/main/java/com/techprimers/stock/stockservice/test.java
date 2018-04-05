9a949b748c0078cd9010ac25e17dd2c6d029f068
panjalasunny'' <panjalasunny>
2018-04-05 18:22:52 -0500

    spring_cloud_stock_service

---
+++ spring_cloud_stock-service/src/main/java/com/techprimers/stock/stockservice/test.java
@@ -0,0 +1,14 @@
+package com.techprimers.stock.stockservice;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = null;
+		String str1="abc";
+		System.out.println(str1.equals("abc") | str.equals(null));
+
+	}
+
+}
